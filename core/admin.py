1cae8b261dabc363e38cb5759e76da811f5581c9
Orwa400 <matheworwa3@gmail.com>
2025-01-10 00:14:04 +0300

    Update Admin Interface

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Store, InventoryItem, Wishlist, Request, Delivery,Rating
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Store)
+admin.site.register(InventoryItem)
+admin.site.register(Wishlist)
+admin.site.register(Request)
+admin.site.register(Delivery)
+admin.site.register(Rating)
+
