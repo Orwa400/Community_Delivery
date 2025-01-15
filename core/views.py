f30b8977d6a0fa163e8abe7f7df26e545efdd67f
Orwa400 <matheworwa3@gmail.com>
2025-01-15 22:19:41 +0300

    Update Views store logic

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Store
+from django.http import HttpResponse
 
 def store_list(request):
     stores = Store.objects.all()
@@ -9,4 +10,24 @@ def store_detail(request,pk):
     store = get_object_or_404(Store, pk=pk)
     return render(request, 'core/store_detail.html', {'store': store})
 
+def store_update(request, pk):
+    store = get_object_or_404(Store, pk=pk)
+    if request.method == 'POST':
+        form = StoreForm(request.POST, instance=store) # Bind form with current store data
+        if form_is.valid():
+            form.save
+            return HttpResponseRedirect('/stores/')
+        else:
+            form = StoreForm(instance=store)  # Pre-fill the form with the current store data
+
+        return render(request, 'core/store_form.html', {'form': form})
+
+def store_delete(request, pk):
+    store = get_object_or_404(Store, pk=pk)
+    if request.method =='POST':
+        store.delete()
+        return HttpResponseRedirect('/stores/')
+    
+    return render(request, 'core/store_confirm_delete.html', context)
+    
 
