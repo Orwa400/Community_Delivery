accdf74f4f37f453bdcbcc0c65cf964bae1d89d4
Orwa400 <matheworwa3@gmail.com>
2025-01-15 22:52:23 +0300

    Add Store Form

--- community_delivery/urls.py
+++ community_delivery/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib import admin
+from core import views
+
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('stores/', views.store_list, name='store-list'),
     path('stores/<int:pk>/', views.store_detail, name='store-detail'),
     path('stores/cretae/', views.store_create, name='store-create'),
