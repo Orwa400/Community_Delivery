9554639336c0f0c1e1bc5567b198c5af2c2551ce
Orwa400 <matheworwa3@gmail.com>
2025-01-15 22:43:10 +0300

    Add API list and Dettail View

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Store
-from django.http import HttpResponse
+from django.http import  HttpResponseRedirect
+from .forms import StoreForm
 
 def store_list(request):
     stores = Store.objects.all()
@@ -9,6 +10,16 @@ def store_list(request):
 def store_detail(request,pk):
     store = get_object_or_404(Store, pk=pk)
     return render(request, 'core/store_detail.html', {'store': store})
+
+def store_create(request):
+    if request.method == 'POST':
+        form = StoreForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpREsponseRedirect('/stores/')
+    else:
+        form = StoreForm()
+    return render(request, 'core/store_form.html', {'form': form})
 
 def store_update(request, pk):
     store = get_object_or_404(Store, pk=pk)
@@ -29,5 +40,17 @@ def store_delete(request, pk):
         return HttpResponseRedirect('/stores/')
     
     return render(request, 'core/store_confirm_delete.html', context)
-    
 
+# API Views
+def store_list_api(request):
+    stores = Store.objects.values()
+    return JsonResponse({'stores': list(stores)})
+
+def store_detail_api(request, pk):
+    store = get_object_or_404(Store, pk=pk)
+    data = {
+        'id': store.id,
+        'name': store.name,
+        'address': store.address,
+    }
+    return JsonResponse(data)
