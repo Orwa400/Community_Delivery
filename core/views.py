3d163bebc0691909cc7b9573b9b566b84aa2619f
Orwa400 <matheworwa3@gmail.com>
2025-01-15 22:00:39 +0300

    Add store views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Store
 
-# Create your views here.
+def store_list(request):
+    stores = Store.objects.all()
+    return render(request, 'core/store_list.html', {'stores': stores})
+
+def store_detail(request,pk):
+    store = get_object_or_404(Store, pk=pk)
+    return render(request, 'core/store_detail.html', {'store': store})
+
+
