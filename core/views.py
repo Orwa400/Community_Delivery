80563579f2a6e30884533afd1cb72632cb7f7de7
Orwa400 <matheworwa3@gmail.com>
2025-02-25 11:52:56 +0300

    Add SignUp,Login and Logout views

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Store
 from django.http import  HttpResponseRedirect, HttpResponse
 from .forms import StoreForm
+from django.contrib.auth import login, logout, authenticate 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def home(request):
     return render(request, 'home.html')
@@ -57,3 +59,29 @@ def store_detail_api(request, pk):
         'address': store.address,
     }
     return JsonResponse(data)
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # log in user automatically
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+        return render(request, 'core/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'core/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
