9c87bc18ae43c581002e613223e3a226dd301123
Orwa400 <matheworwa3@gmail.com>
2025-02-26 15:37:07 +0300

    Update URLs

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('stores/cretae/', views.store_create, name='store-create'),
     path('stores/<int:pk>/update/', views.store_update, name='store-update'),
     path('stores/<int:pk>/delete/', views.store_delete, name='store-delete'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
